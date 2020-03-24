f38bfe64c102c3f332c5de084bfe7105e49f256e
Morlay <morlay.null@gmail.com>
2020-03-24 15:18:32 +0800

    fix(form-controls): fix ButtonGroup

--- @querycap/uikit/overlays/__examples__/Overlay.tsx
+++ @querycap/uikit/overlays/__examples__/Overlay.tsx
@@ -1,58 +1,57 @@
 import { Overlay } from "@querycap/uikit";
 import React, { useRef } from "react";
 
-const Placement = ({ placement }: { placement: string }) => {
+export const Overlays = () => {
   const ref = useRef(null);
 
   return (
-    <>
+    <div
+      css={{
+        padding: "20em",
+        fontSize: 10,
+      }}>
       <div
         ref={ref}
         css={{
           border: `1px solid #ddd`,
-          padding: "1em",
+          width: "30em",
+          height: "30em",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}>
-        {placement}
+        Trigger
       </div>
-      <Overlay triggerRef={ref} placement={placement as any}>
-        <div
-          css={{
-            border: `1px solid #ddd`,
-            backgroundColor: `#eee`,
-            padding: "2em",
-          }}>
-          {placement}
-        </div>
-      </Overlay>
-    </>
+      {[
+        "left",
+        "right",
+        "bottom",
+        "top",
+        "left-top",
+        "left-bottom",
+        "right-top",
+        "right-bottom",
+        "top-left",
+        "bottom-left",
+        "top-right",
+        "bottom-right",
+      ].map((placement) => (
+        <Overlay key={placement} triggerRef={ref} placement={placement as any}>
+          <div
+            css={{
+              fontSize: 10,
+              border: `1px solid #ddd`,
+              backgroundColor: `#eee`,
+              width: "8em",
+              height: "8em",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}>
+            {placement}
+          </div>
+        </Overlay>
+      ))}
+    </div>
   );
 };
-
-export const Overlays = () => (
-  <div
-    css={{
-      display: "flex",
-      flexWrap: "wrap",
-      justifyContent: "space-between",
-      "& > *": { width: "30%", padding: "6em" },
-    }}>
-    {[
-      "left",
-      "right",
-      "bottom",
-      "top",
-      "left-top",
-      "left-bottom",
-      "right-top",
-      "right-bottom",
-      "top-left",
-      "bottom-left",
-      "top-right",
-      "bottom-right",
-    ].map((placement) => (
-      <div key={placement}>
-        <Placement placement={placement} />
-      </div>
-    ))}
-  </div>
-);
