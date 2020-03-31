dc91ec7983130355667eca8ce24c6dc5b0c4619e
Morlay <morlay.null@gmail.com>
2020-03-31 14:46:12 +0800

    fix: re-exports fix

--- @querycap-ui/core/index.ts
+++ @querycap-ui/core/index.ts
@@ -1,4 +1,6 @@
 export * from "polished";
-export * from "react-spring";
 export * from "./theme";
 export * from "./select";
+
+import { animated, config, interpolate, useChain, useSpring, useSprings, useTrail, useTransition } from "react-spring";
+export { animated, useSpring, useChain, useTrail, useTransition, useSprings, interpolate, config };
