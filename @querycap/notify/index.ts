0a2c689b23a8c85d1b2685e2e02978a960e87259
Morlay <morlay.null@gmail.com>
2020-04-03 14:34:53 +0800

    feat(notify): struct updates

--- @querycap/notify/index.ts
+++ @querycap/notify/index.ts
@@ -1,1 +1,73 @@
-export * from "./src";
+import { Actor, useConn, useObservable, useStore } from "@reactorx/core";
+import { omit } from "lodash";
+import { useMemo } from "react";
+import { v4 as uuid } from "uuid";
+
+export enum Type {
+  success = "success",
+  info = "info",
+  error = "error",
+  warning = "warning",
+}
+
+export interface MsgInfo {
+  summary: string;
+  description?: string;
+}
+
+export interface Msg extends MsgInfo {
+  id: string;
+  type: keyof typeof Type;
+}
+
+export interface MsgSet {
+  [id: string]: Msg;
+}
+
+const InfoActor = Actor.of("messages");
+
+export const notify = InfoActor.named<Omit<Msg, "id">>("message").effectOn<MsgSet>(
+  InfoActor.group,
+  (messages, actor) => {
+    const message = {
+      ...actor.arg,
+      id: uuid(),
+    };
+
+    return {
+      ...messages,
+      [message.id]: message,
+    };
+  },
+);
+
+export const closeMsg = InfoActor.named<string>("message/close").effectOn<MsgSet>(
+  InfoActor.group,
+  (messages, actor) => {
+    return omit(messages, [actor.arg]);
+  },
+);
+
+export const useNotify = () => {
+  const store$ = useStore();
+
+  return useMemo(() => {
+    return (type: keyof typeof Type, summary: string, description?: string) => {
+      notify
+        .with({
+          type,
+          summary,
+          description,
+        })
+        .invoke(store$);
+    };
+  }, []);
+};
+
+export const useMessages$ = () => {
+  return useConn(useStore(), (state: any = {}): MsgSet => state[InfoActor.group] || {}, []);
+};
+
+export const useMessages = () => {
+  return useObservable(useMessages$());
+};
--- @querycap/request/src/apiErrorNotify.ts
+++ @querycap/request/src/apiErrorNotify.ts
@@ -1,4 +1,4 @@
-import { IMsgWithType, notify } from "@querycap/notify";
+import { Msg, notify } from "@querycap/notify";
 import { Actor } from "@reactorx/core";
 import { Observable } from "rxjs";
 import { filter, map } from "rxjs/operators";
@@ -8,14 +8,14 @@ import { IStatusError } from "./StatusError";
 export const hasApiGlobalError = (data: IStatusError) => data && data.canBeTalkError && !!data.msg;
 
 export const createApiErrorNotify = () => {
-  return (action$: Observable<RequestActor>): Observable<Actor<IMsgWithType>> =>
+  return (action$: Observable<RequestActor>): Observable<Actor<Omit<Msg, "id">>> =>
     action$.pipe(
       filter((actor) => {
         return RequestActor.isFailedRequestActor(actor) && hasApiGlobalError(actor.arg.data);
       }),
       map((action) => {
         const { msg, desc } = action.arg.data;
-        return notify.with({ type: "danger", summary: msg, description: desc });
+        return notify.with({ type: "error", summary: msg, description: desc });
       }),
     );
 };
