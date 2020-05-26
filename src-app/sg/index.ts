2f7da5cd482ea7567a79ef532f2faa4481a24f6e
Morlay <morlay.null@gmail.com>
2020-05-26 14:14:13 +0800

    chore(deps): updates

--- src-app/sg/index.ts
+++ src-app/sg/index.ts
@@ -1,3 +1,10 @@
-import("./bootstrap").then(({ bootstrap }) => {
-  bootstrap(document.querySelector("#root") as Element, true);
-});
+const run = () =>
+  import("./bootstrap").then(({ bootstrap }) => {
+    bootstrap(document.querySelector("#root") as Element, true);
+  });
+
+if ("serviceWorker" in navigator) {
+  void navigator.serviceWorker.register("/sw.js").then(() => run());
+} else {
+  void run();
+}
