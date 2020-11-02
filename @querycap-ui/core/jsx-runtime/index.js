f2caa9627e03578bd1b3a535fb0ccba47ee1f457
Morlay <morlay.null@gmail.com>
2020-11-02 11:36:28 +0800

    chore(deps): updates

--- @querycap-ui/core/jsx-runtime/index.js
+++ @querycap-ui/core/jsx-runtime/index.js
@@ -1,13 +1,3 @@
-/* eslint-disable */
-var _jsx = require("@emotion/core").jsx;
-var assign = require("@babel/runtime-corejs3/core-js/object/assign");
-
-function jsx(Component, props, maybeKey) {
-  const children = props.children;
-  delete props.children;
-  return _jsx.apply(null, [Component, maybeKey ? assign(props, { key: maybeKey }) : props].concat(children));
-}
-
-exports.jsx = jsx;
-exports.jsxs = jsx;
-exports.Fragment = require("react").Fragment;
+exports.Fragment = require("@emotion/core/jsx-runtime").Fragment;
+exports.jsx = require("@emotion/core/jsx-runtime").jsx;
+exports.jsxs = require("@emotion/core/jsx-runtime").jsxs;
