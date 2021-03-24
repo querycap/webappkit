ed1c0f4784dd24cde74aef75961bd20cf553e72d
Morlay <morlay.null@gmail.com>
2021-03-24 13:31:43 +0800

    feat(devkit): support pnpm

--- @querycap-dev/devkit/src/action-build.ts
+++ @querycap-dev/devkit/src/action-build.ts
@@ -1,49 +1,50 @@
-import { generate } from "@querycap-dev/generate";
-import { dump } from "js-yaml";
-import { isUndefined, omitBy } from "lodash";
-import { join } from "path";
-import { stringify } from "querystring";
-import { IState } from "./state";
+import {generate} from "@querycap-dev/generate";
+import {dump} from "js-yaml";
+import {isUndefined, omitBy} from "lodash";
+import path, {join} from "path";
+import {stringify} from "querystring";
+import {IState} from "./state";
+import * as fs from "fs";
 
 const omitEmpty = <T extends object = any>(o: T) => omitBy(o, (v) => isUndefined(v));
 
 export const writeConfig = (cwd: string, state: IState) => {
-  const baseConfig = {
-    APP: state.name,
-    ENV: state.env,
+    const baseConfig = {
+        APP: state.name,
+        ENV: state.env,
 
-    // for overwrite
-    PROJECT_NAME: `web-${state.name}`,
-    PROJECT_GROUP: state.project.group,
-    PROJECT_DESCRIPTION: state.meta.manifest?.name,
-  };
+        // for overwrite
+        PROJECT_NAME: `web-${state.name}`,
+        PROJECT_GROUP: state.project.group,
+        PROJECT_DESCRIPTION: state.meta.manifest?.name,
+    };
 
-  generate(
-    join(cwd, `./config/default.yml`),
-    dump(
-      omitEmpty({
-        ...baseConfig,
-        APP_CONFIG: stringify(state.meta.config || {}, ",", "=", {
-          encodeURIComponent: (v) => v,
-        }),
-      }),
-    ),
-  );
+    generate(
+        join(cwd, `./config/default.yml`),
+        dump(
+            omitEmpty({
+                ...baseConfig,
+                APP_CONFIG: stringify(state.meta.config || {}, ",", "=", {
+                    encodeURIComponent: (v) => v,
+                }),
+            }),
+        ),
+    );
 
-  generate(
-    join(cwd, `./config/master.yml`),
-    dump(
-      omitEmpty({
-        APP_CONFIG: stringify(state.meta.config$ || {}, ",", "=", {
-          encodeURIComponent: (v) => v,
-        }),
-      }),
-    ),
-  );
+    generate(
+        join(cwd, `./config/master.yml`),
+        dump(
+            omitEmpty({
+                APP_CONFIG: stringify(state.meta.config$ || {}, ",", "=", {
+                    encodeURIComponent: (v) => v,
+                }),
+            }),
+        ),
+    );
 
-  generate(
-    join(cwd, `./deploy/qservice.yml`),
-    `apiVersion: serving.octohelm.tech/v1alpha1
+    generate(
+        join(cwd, `./deploy/qservice.yml`),
+        `apiVersion: serving.octohelm.tech/v1alpha1
 kind: QService
 spec:
   image: \${{ PROJECT_IMAGE }}
@@ -62,11 +63,11 @@ spec:
     initialDelaySeconds: 5
     periodSeconds: 5
 `,
-  );
+    );
 
-  generate(
-    join(cwd, `./Dockerfile`),
-    `
+    generate(
+        join(cwd, `./Dockerfile`),
+        `
 ARG DOCKER_REGISTRY
 FROM --platform=\${BUILDPLATFORM} \${DOCKER_REGISTRY}/docker.io/library/node:15-buster as build-env
 
@@ -84,16 +85,23 @@ ARG PROJECT_VERSION
 
 ARG YARN_NPM_REGISTRY_SERVER
 ARG YARN_HTTPS_PROXY
+
+${fs.existsSync(path.join(state.cwd, "pnpm-lock.yaml")) ? `
+RUN npm install -g --registry=\${YARN_NPM_REGISTRY_SERVER} pnpm
+RUN --mount=type=cache,id=\${PROJECT_GROUP}-\${PROJECT_NAME},target=/root/.pnpm-store/ \\
+    pnpm --registry=\${YARN_NPM_REGISTRY_SERVER} install
+` : `
 ENV YARN_CACHE_FOLDER=/tmp/yarn-cache
+RUN --mount=type=cache,id=\${PROJECT_GROUP}-\${PROJECT_NAME},target=\${YARN_CACHE_FOLDER} yarn install
+`}
 
-RUN --mount=type=cache,id=\${PROJECT_GROUP}-\${PROJECT_NAME},target=\${YARN_CACHE_FOLDER} yarn
-
-RUN PROJECT_GROUP=\${PROJECT_GROUP} PROJECT_VERSION=\${PROJECT_VERSION} yarn devkit build --prod \${APP} \${ENV}
+RUN PROJECT_GROUP=\${PROJECT_GROUP} PROJECT_VERSION=\${PROJECT_VERSION} \\
+    npx devkit build --prod \${APP} \${ENV}
 
 FROM \${DOCKER_REGISTRY}/docker.io/querycap/webappserve:0.0.0
 
 ARG PROJECT_NAME
 COPY --from=builder /src/public/\${PROJECT_NAME} /app
 `,
-  );
+    );
 };
