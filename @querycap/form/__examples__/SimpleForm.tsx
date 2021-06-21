ca1cabdef0aeb8c508de3f3320a44e47156e21bd
chenbenzheng <chenbenzheng@rockontrol.com>
2021-06-21 17:55:59 +0800

    fix(validators): when default value is number, isEmpty return true

--- @querycap/form/__examples__/SimpleForm.tsx
+++ @querycap/form/__examples__/SimpleForm.tsx
@@ -28,6 +28,7 @@ export const SimpleForm = () => {
     name: "231",
     email: "xxx@x.com",
     gender: "FEMALE",
+    number: 123,
   });
 
   return (
@@ -46,6 +47,10 @@ export const SimpleForm = () => {
         <FormControlWithField name="email" desc="邮箱" validate={pipe(required(), validEmail())}>
           {SimpleInputText}
         </FormControlWithField>
+        <FormControlWithField name="number" validate={pipe(required())}>
+          {SimpleInputText}
+        </FormControlWithField>
+
         <FormControls>
           <Button type="submit" primary>
             Submit
